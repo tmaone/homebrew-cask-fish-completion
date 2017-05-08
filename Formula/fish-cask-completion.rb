869a102152b032af198361e39cbc3452a786abed
tmaone <marios.daoutis@gmail.com>
2017-05-08 09:34:47 +0200

    cask completion typo

--- Formula/fish-cask-completion.rb
+++ Formula/fish-cask-completion.rb
@@ -3,7 +3,7 @@ class FishCaskCompletion < Formula
    homepage "https://github.com/tmaone/homebrew-cask-fish-completion"
    version '0.0.1'
    url "https://github.com/tmaone/homebrew-cask-fish-completion/archive/#{version}.tar.gz"
-   
+
    head do
      url "https://github.com/tmaone/homebrew-cask-fish-completion.git"
    end
@@ -12,6 +12,6 @@ class FishCaskCompletion < Formula
 
    def install
       fish_function.install "cask.fish"
-      fish_completion.install "cask-completion.fish" => "fisher.fish"
+      fish_completion.install "cask-completion.fish" => "cask.fish"
    end
  end
