72cf5b8ca743d148d7e946f56c0bcf71d8090036
tmaone <marios.daoutis@gmail.com>
2017-05-08 09:40:09 +0200

    rearrange formula name

--- Formula/cask-fish-completion.rb
+++ Formula/cask-fish-completion.rb
@@ -1,13 +1,14 @@
-class FishCaskCompletion < Formula
+class CaskFishCompletion < Formula
    desc "Fish completion for the brew-cask command"
    homepage "https://github.com/tmaone/homebrew-cask-fish-completion"
    version '0.0.1'
    url "https://github.com/tmaone/homebrew-cask-fish-completion/archive/#{version}.tar.gz"
-
+   
    head do
      url "https://github.com/tmaone/homebrew-cask-fish-completion.git"
    end
 
+   bottle :unneeded
    depends_on "fish"
 
    def install
